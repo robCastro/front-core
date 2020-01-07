c990db2bd84778c810aaa62aeeaad978cf2a3689
Roberto Castro <roberto.ernesto.castro@gmail.com>
2020-01-07 09:37:37 -0600

    modificado:     src/app/pages/detalle-mercancia/detalle-mercancia.component.html 	modificado:     src/app/pages/detalle-mercancia/detalle-mercancia.component.ts Muestra de errores implementada

--- src/app/pages/detalle-mercancia/detalle-mercancia.component.ts
+++ src/app/pages/detalle-mercancia/detalle-mercancia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import Swal from 'sweetalert2';
 
 import { MercanciaService } from '../../services/mercancia.service';
 
@@ -27,18 +28,60 @@ export class DetalleMercanciaComponent implements OnInit {
 		
 		this.route.queryParamMap.subscribe(params => {
 			let id_mercancia:number = parseInt(params.get('id_mercancia'));
-			this.mercanciaService.getMercancia(id_mercancia).subscribe(mercancia => {
-				this.mercancia = mercancia;
-				this.isMercanciaLoaded = true;
-			});
-			this.mercanciaService.getDetalles(id_mercancia).subscribe(respuesta => {
-				this.detalles = respuesta.detalles;
-				this.isDetallesLoaded = true;
-				// Aqui mismo display de errores que vienen con respuesta
-				console.log(this.detalles);
-			});
+			this.mercanciaService.getMercancia(id_mercancia).subscribe(
+				mercancia => {
+					this.mercancia = mercancia;
+					this.isMercanciaLoaded = true;
+				}, err => {
+					this.displayError(err);
+					this.mercancia = null;
+				}
+			);
+			this.mercanciaService.getDetalles(id_mercancia).subscribe(
+				respuesta => {
+					this.detalles = respuesta.detalles;
+					this.isDetallesLoaded = true;
+					console.log('Errores: ', respuesta.errores);
+					if(respuesta.errores)
+						this.displayError({error: {msg: respuesta.errores}});
+					console.log(this.detalles);
+				}, error => {
+					this.displayError(error);
+				}
+			);
 		});
 
+	}
+
+	private displayError(err: any){
+		console.log(err);
+		let msg = "";
+		err.error.msg ? msg = err.error.msg : msg = err.message;
+		console.log(msg);
+		Swal.fire({
+			title: '<strong>Error!</strong>',
+			html: msg,
+			icon: 'error',
+			confirmButtonText: 'OK'
+		});
+		
+	}
+
+	private displayProcesando(abrir: boolean){
+		if(abrir){
+			Swal.fire({
+				position: 'top-end',
+				title: 'Procesando',
+				html: '...',
+				icon: 'info',
+				showConfirmButton: false,
+				allowOutsideClick: false,
+  				allowEscapeKey: false,
+			});
+		}
+		else{
+			Swal.close();
+		}
 	}
 
 }

e4ed9f0677974d7ba3b7910f704d99d021f12b1c
Roberto Castro <roberto.ernesto.castro@gmail.com>
2020-01-09 13:35:10 -0600

    modificado:     src/app/services/url_back.ts Uso de wss para ssl

--- src/app/services/url_back.ts
+++ src/app/services/url_back.ts
@@ -1,5 +1,5 @@
 export const desarrollo:string = 'http://localhost:3000/api/core/';
 export const produccion:string = 'https://igfcore.herokuapp.com/api/core/';
 
-export const wsProduccion:string = 'ws://igfcore.herokuapp.com/';
+export const wsProduccion:string = 'wss://igfcore.herokuapp.com/';
 export const wsDesarrollo:string = 'ws://localhost:3000/';
